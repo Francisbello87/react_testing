a3174946a9ec4e72d1fb5839178cdcc918dc3033
Francis Bello <bellofrancis87@gmail.com>
2023-11-28 11:05:35 +0100

    FindByText method

--- __tests_/Homepage.test.jsx
+++ __tests_/Homepage.test.jsx
@@ -1,29 +1,52 @@
 import Home from "@/app/page";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-describe('Home Page - Rendering', () => {
-  it('should have Home Page text', () =>{
-    render(<Home/>);
-    expect(screen.getByText("Home Page")).toBeInTheDocument();
+describe("Home Page", () => {
+  describe("Rendering", () => {
+    it("should have Home Page text", () => {
+      render(<Home />);
+      expect(screen.getByText("Home Page")).toBeInTheDocument();
+    });
+
+    it("should have button with text Click Here", () => {
+      render(<Home />);
+      expect(
+        screen.getByRole("button", { name: "Click Here" })
+      ).toBeInTheDocument();
+    });
+
+    it("should have input field with label Enter Random Text", () => {
+      render(<Home />);
+      // expect(screen.getByRole("textbox")).toBeInTheDocument(); with role
+      expect(screen.getByLabelText(/Enter any text/)).toBeInTheDocument(); //with label
+    });
+    it("should have input field with placeholder Search", () => {
+      render(<Home />);
+      expect(screen.getByPlaceholderText(/Search/)).toBeInTheDocument();
+    });
+    it("should have input field by display value", () => {
+      render(<Home />);
+      expect(screen.getByDisplayValue(/Arsenal/)).toBeInTheDocument();
+    });
+    it("should not find element with text: This is the text", () => {
+      render(<Home />);
+      expect(screen.queryByText("This is the text")).not.toBeInTheDocument();
+    });
   });
-
-  it("should have button with text Click Here", () => {
-    render(<Home/>)
-    expect(screen.getByRole("button", { name: "Click Here" })).toBeInTheDocument();
+  describe("Behaviour", () => {
+    it("should click on Show Text button and find new text", async () => {
+      render(<Home />);
+      expect(screen.queryByText('This is the text!')).not.toBeInTheDocument()
+      const showTextButton = screen.getByRole('button', {name: 'Show Text'})
+      await userEvent.click(showTextButton)
+      // expect(screen.getByText("This is the text!")).toBeInTheDocument();
+      expect(await screen.findByText("This is the text!", {}, {timeout: 5000})).toBeInTheDocument();
+      // await waitFor(() => {
+      //   expect(screen.getByText("This is the text!")).toBeInTheDocument();
+      // }, {timeout: 1200})
+    });
   });
-
-  it('should have input field with label Enter Random Text', () => {
-    render(<Home/>)
-    // expect(screen.getByRole("textbox")).toBeInTheDocument(); with role
-    expect(screen.getByLabelText(/Enter any text/)).toBeInTheDocument() //with label
-  })
-  it('should have input field with placeholder Search', () => {
-    render(<Home/>)
-    expect(screen.getByPlaceholderText(/Search/)).toBeInTheDocument();
-  })
-  it('should have input field by display value', () => {
-    render(<Home/>)
-    screen.getByDisplayValue(/Arsenal/)
-    
-  })
 });
+
+// use findBy query whenever you have state updating e.g modal,
--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 "use client";
 
+import { useState } from "react";
+
 export default function Home() {
+  const [showText, setShowText] = useState(false);
   return (
     <main>
       <h1>Home Page</h1>
@@ -10,10 +13,23 @@ export default function Home() {
         <input id="randomText" type="text" />
       </div>
       <div>
-        <input  type="text" placeholder="Search...." />
+        <input type="text" placeholder="Search...." />
       </div>
       <div className=" mt-4 text-black">
-        <input value="Arsenal FC"  />
+        <input value="Arsenal FC" />
+      </div>
+      <div className=" flex gap-4 justify-start mt-5">
+        {showText && <span>This is the text!</span>}
+        <button
+          className="bg-gray-600"
+          onClick={() =>
+            setTimeout(() => {
+              setShowText(!showText);
+            }, 1100)
+          }
+        >
+          Show Text
+        </button>
       </div>
     </main>
   );
